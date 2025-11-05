b4cf7212bb62b4b3dfeae1cabb9b4965a1929074
Daksh Sawhney <darkstardaksh@gmail.com>
2025-11-05 13:21:48 +0530

    SG Created for ECS IAM Role

--- infrastructure/security.tf
+++ infrastructure/security.tf
@@ -18,3 +18,30 @@ resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
   role = aws_iam_role.ecs_task_execution_role
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
+
+###############################
+# Security Group for ECS Role
+###############################
+resource "aws_security_group" "web_sg" {
+  name = "${var.project_name}-sg"
+  description = "Allow HTTP and SSH access"
+  vpc_id = module.vpc.vpc_id
+
+  ingress = {
+    description = "Allow HTTP from anywhere"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1" # -1 signifies all protocols
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = local.common_tags
+}
