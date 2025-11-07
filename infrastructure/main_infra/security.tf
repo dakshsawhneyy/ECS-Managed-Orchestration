16a140731eca6d15f8f3b5c0e7bed104b34c8e1b
Daksh Sawhney <darkstardaksh@gmail.com>
2025-11-07 12:24:57 +0530

    SG added for ALB

--- infrastructure/main_infra/security.tf
+++ infrastructure/main_infra/security.tf
@@ -45,3 +45,43 @@ resource "aws_security_group" "web_sg" {
 
   tags = local.common_tags
 }
+
+###############################
+# Security Group for ALB
+###############################
+resource "aws_security_group" "alb_sg" {
+  name        = "${var.project_name}-alb-sg"
+  description = "Allow HTTP and SSH access to load balancer"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    description = "Allow HTTP from anywhere"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "Allow HTTPS from anywhere"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "Allow HTTPS from anywhere"
+    from_port   = 9000
+    to_port     = 9000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1" # -1 signifies all protocols
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = local.common_tags
+}
