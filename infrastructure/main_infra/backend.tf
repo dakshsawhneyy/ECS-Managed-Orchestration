1339ed6a6880a7b018c8aab9b4184cc38de34a11
Daksh Sawhney <darkstardaksh@gmail.com>
2025-11-07 10:57:05 +0530

    dynamodb table lock name fixed

--- infrastructure/main_infra/backend.tf
+++ infrastructure/main_infra/backend.tf
@@ -3,6 +3,6 @@ terraform {
     bucket = "ecs-managed-orchestration-sf"
     region = "ap-south-1"
     key = "daksh/terraform.tfstate"
-    dynamodb_table = "terraform_lock"
+    dynamodb_table = "terraform-lock"
   }
 }
