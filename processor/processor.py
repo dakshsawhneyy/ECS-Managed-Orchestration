a3f37acfa0916af6c2f7757ba8d054c115584030
Daksh Sawhney <darkstardaksh@gmail.com>
2025-11-09 17:31:05 +0530

    wrote processor.py to send logs SQS->DynamoDB

---
+++ processor/processor.py
@@ -0,0 +1,70 @@
+import boto3
+import requests
+import json
+import os
+import time
+import uuid
+
+# env's
+SQS_URL = os.getenv("SQS_URL")
+DYNAMODB_TABLE = os.getenv("DYNAMODB_TABLE")
+
+sqs = boto3.client('sqs', region_name='ap-south-1')
+dynamodb = boto3.resource('dynamodb', region_name='ap-south-1')
+table = dynamodb.table(DYNAMODB_TABLE)
+
+
+# Read logs from SQS
+def read_from_sqs():
+    try:
+        response = sqs.recieve_message(
+            QueueUrl=QUEUE_URL,
+            MaxNumberOfMessages=10,         # fetch up to 10 at once
+            WaitTimeSeconds=10,             # enable long polling
+            VisibilityTimeout=30            # gives time to process before retry
+        )
+        print('Response: ', response)       # Debugging
+        messages = response.get('Messages', [])
+        print('Messages: ', messages)
+        return messages
+    except Exception as e:
+        print(f"Error receiving messages: {str(e)}")
+        return []
+    
+    
+# Function to write logs in DynamoDB
+def write_logs_to_dynamodb(log_data):
+    try:
+        table.put_item(Item=log_data)
+    except Exception as e:
+        print(f"Error inserting into DynamoDB: {str(e)}")
+        
+# Delete messages from SQS, once they been inserted into DynamoDB
+def delete_msgs_from_sqs(reciept_handle):
+    try:
+        sqs.delete_message(QueueUrl=QUEUE_URL, ReceiptHandle=receipt_handle)
+        print("Message deleted from SQS")
+    except Exception as e:
+        print(f"Error deleting messages from SQS: {str(e)}")
+        
+
+# Main Function
+def main():
+    print('Processor Started')
+    messages = read_from_sqs()
+    if not messages:
+        print("No messages in SQS, waiting...")
+        time.sleep(5)
+        continue
+    
+    for msg in messages:
+        try:
+            log_data = json.loads(msg['Body'])
+            write_logs_to_dynamodb(log_data)
+            delete_msgs_from_sqs(msg['ReceiptHandle'])
+        except Exception as e:
+            print(f"Error processing message: {str(e)}")
+            
+
+if __name__ == "__main__":
+    main()
